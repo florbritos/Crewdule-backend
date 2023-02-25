818f47d3cd749641ce9c309c0a2090df788a8824
florbritos <flor-britos@hotmail.com.ar>
2023-02-25 14:23:47 -0300

    testing sign in

--- api/controllers/users.api.controller.js
+++ api/controllers/users.api.controller.js
@@ -199,14 +199,25 @@ function updatePassword(req, res){
 }
 
 function checkSession(req,res){
-    const token = req.cookies['Crewdule-Auth']
+    
+    res.cookie('Test', 'hola', {httpOnly: true, secure: true})
 
-    if (!token) {
+    try{
+        const token = req.cookies['Crewdule-Auth']
+        res.status(200).json({ message: 'Token found in cookies. Session opened' })
+    }
+    catch{
         return res.status(401).json({ message: 'No token in cookies. No session opened' })
     }
+        
 
-    res.status(200).json({ message: 'Token found in cookies. Session opened' })
+    // if (!token) {
+    //     return res.status(401).json({ message: 'No token in cookies. No session opened' })
+    // }
+
+    // res.status(200).json({ message: 'Token found in cookies. Session opened' })
 }
+
 export {
     login,
     logout,
