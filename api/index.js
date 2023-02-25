1b450450b8e9970284018287b0ef454766e2fed3
florbritos <flor-britos@hotmail.com.ar>
2023-02-25 14:36:26 -0300

    adding cookieparser a index.js

--- api/index.js
+++ api/index.js
@@ -3,10 +3,12 @@ import cors from 'cors'
 import CalendarRoutes from './routers/calendar.api.routes.js'
 import UsersRoutes from './routers/users.api.routes.js'
 import DocumentsRoutes from './routers/documents.api.routes.js'
+import cookieParser from "cookie-parser";
 
 process.env.TZ = 'America/Argentina/Buenos_Aires'
 
 const app = express()
+app.use(cookieParser())
 app.use(cors({origin: 'https://crewdule.vercel.app', credentials: true}))
 app.use(express.json())
 
