cc37f167767e9fb75cbe56103b7f61e09eb378dd
florbritos <flor-britos@hotmail.com.ar>
2023-02-27 20:52:50 -0300

    changing domain for cookie

--- api/controllers/users.api.controller.js
+++ api/controllers/users.api.controller.js
@@ -19,7 +19,7 @@ function login(req, res) {
             const token = JWTService.createJWTToken(user)
             TokenService.addToDB({ token, user_id: user._id })
             console.log('entro2')
-            res.cookie('Crewdule-Auth', token, {domain: ".vercel.com", httpOnly: true, secure: true})
+            res.cookie('Crewdule-Auth', token, {domain: "example.com", httpOnly: true, secure: true})
             console.log('entro3')
             res.status(200).json(user)
         })
